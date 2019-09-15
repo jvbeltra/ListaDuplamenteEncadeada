c54a27d87e7371a691388b1f35a108215d4bd925
Joao Beltramini <jvbeltra2011@gmail.com>
2019-09-15 17:23:16 -0300

    First commit

---
+++ ListaDuplamenteEncadeada2/src/listaduplamenteencadeada/Caixa.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listaduplamenteencadeada;
+
+/**
+ *
+ * @author Joao
+ */
+public class Caixa {
+    private Pessoa pessoa;
+    private Caixa anterior;
+    private Caixa proxima;
+
+    public Caixa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Caixa getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Caixa anterior) {
+        this.anterior = anterior;
+    }
+
+    public Caixa getProxima() {
+        return proxima;
+    }
+
+    public void setProxima(Caixa proxima) {
+        this.proxima = proxima;
+    }
+}
---
+++ ListaDuplamenteEncadeada2/src/listaduplamenteencadeada/Pessoa.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listaduplamenteencadeada;
+
+/**
+ *
+ * @author Joao
+ */
+public class Pessoa {
+    private String nome;
+    private int codigo;
+
+    public Pessoa(String nome, int codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+}
